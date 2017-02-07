7fde7fd1bf296b0849d94a37aae2674ff16eb53f
Eric Brewer <ebrewer@autonetmobile.com>
2017-02-06 16:36:48 -0800

    Attempted to alleviate issue by adding `android.id="..."`, but to no avail.

--- app/pages/detail.component.ts
+++ app/pages/detail.component.ts
@@ -1,7 +1,20 @@
 import { Component } from "@angular/core";
+import { RouterExtensions } from "nativescript-angular/router";
 
 @Component({
     templateUrl: "pages/detail.html"
 })
 export class DetailComponent {
+
+    constructor(
+        private routerExtensions: RouterExtensions
+    ) {
+    }
+
+    /**
+     * Only triggers on Android.
+     */
+    onNavigationButtonTapped(event) {
+        this.routerExtensions.back();
+    }
 }
